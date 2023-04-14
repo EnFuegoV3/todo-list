6a316db86a42b0041fb24a250c42c1431d841038
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-04-13 19:47:02 -1000

    creation of new project done, need to work on local storage

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { newProject } from './newProject';
 
 
+const projects = []
 
+
+const addProject = document.querySelector('.add-project');
+
+
+addProject.addEventListener('click', () => {
+    newProject();
+});
---
+++ newProject.js
@@ -0,0 +1,20 @@
+export
+
+function newProject() {
+    const proj = document.createElement('div');
+    const projBtn = document.createElement('button')
+    const view = document.querySelector('.view');
+    const projList = document.querySelector('.project-list');
+    let name = prompt("Enter Project Name");
+    proj.classList.add('project-view');
+    proj.innerHTML = `
+                <h3><input type="text" value="${name}" class="project-name"></h3>
+                <button class="add-todo">&plus; Add new Todo</button>
+            `
+    view.appendChild(proj);
+    
+    projBtn.classList.add('project');
+    projBtn.textContent = `${name}`
+    projList.appendChild(projBtn);
+
+}
